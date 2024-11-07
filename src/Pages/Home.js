ae975ed5dd0c30b3b4761dcb8dc0b4ab4315c216
knbzbr <knbzbr>
2024-11-07 15:46:28 -0300

    Home 80%

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,58 +1,46 @@
 import React from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, Image, Touchable, TouchableOpacity } from 'react-native'
 
 
 export default function Home() {
   return (
-    <View>
-      <View style={css.header}>
-        <Image style={css.logo} source={require("../../assets/LogoHealthSolutions.png")} />
-      </View>
+    <View style={css.container}>
       <View style={css.body}>
         <View>
           <Image style={css.banner} source={require("../../assets/Bicudo.png")} />
         </View>
         <Text style={css.Cnh}>Conheça nossos profissionais</Text>
         <View style={css.fotosPrf}>
-          <View>
-            <Image style={css.prf} source={require("../../assets/Psicologa.png")} />
+          <View style={css.cxPrf}>
+            <Image style={css.prf} source={require("../../assets/psico.jpg")} />
+            <Text style={css.txt}>Psicóloga</Text>
           </View>
-          <View>
+          <View style={css.cxPrf}>
             <Image style={css.prf} source={require("../../assets/TerapiaOcupacional.jpeg")} />
+            <Text style={css.txt}>Terapeuta </Text>
           </View>
-          <View>
-            <Image style={css.prf} source={require("../../assets/Fonoaudiologa.png")} />
+          <View style={css.cxPrf}>
+            <Image style={css.prf} source={require("../../assets/fono.jpg")} />
+            <Text style={css.txt}>Fonoaudiólogo</Text>
+          </View >
+          <View style={css.cxPrf}>
+            <Image style={css.prfp} source={require("../../assets/Fisioterapeuta.jpeg")} />
+            <Text style={css.txt}>Fisioterapeuta</Text>
+          </View>
+          <View style={css.cxPrf}>
+            <Image style={css.prfs} source={require("../../assets/Pilates.jpeg")} />
+            <Text style={css.txt}>Pilates</Text>
           </View>
         </View>
-        <View style={css.fotosPrf}>
-          <View>
-            <Image style={css.prf} source={require("../../assets/Fisioterapeuta.jpeg")} />
-          </View>
-          <View>
-            <Image style={css.prf} source={require("../../assets/Pilates.jpeg")} />
-          </View>
-        </View>
+        <TouchableOpacity style={css.Agnd}>
+          <Text style={css.btnTxt}>Agende já sua consulta</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
 }
 
 const css = StyleSheet.create({
-  header: {
-    width: "100%",
-    height: 120,
-    backgroundColor: "#4796A8",
-    marginTop: "100px",
-    textAlign: "center",
-    alignItems: "center",
-    justifyContent: "flex-end",
-
-  },
-  logo: {
-    height: 130,
-    width: 140,
-    marginBottom: -24
-  },
   banner: {
     width: "100%",
     height: 200,
@@ -67,10 +55,43 @@ const css = StyleSheet.create({
     display:"flex",
     flexDirection:"row",
     justifyContent: "space-around",
+    flexWrap:"wrap",
+    marginTop:40
   },
   prf:{
-    width:100,
+    width:110,
     height:110,
-
+    borderRadius:100
+  },
+  prfs:{
+    width:110,
+    height:110,
+    borderRadius:100,
+  },
+  prfp:{
+    width:110,
+    height:110,
+    borderRadius:100,
+  },
+  Agnd:{
+    width:170,
+    height:40,
+    backgroundColor:"#4796A8",
+    borderRadius:4,
+    textAlign:"center",
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:30,
+    marginLeft:120,
+  },
+  cxPrf:{
+    textAlign:"center",
+    alignItems:"center",
+  },
+  txt:{
+    marginBottom:30
+  },
+  btnTxt:{
+    color:"white"
   }
 })
