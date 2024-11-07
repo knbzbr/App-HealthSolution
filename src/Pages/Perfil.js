039745380b4170e526a6b92a1e69df5c2119bf63
LirySouza <lirianysouza15@gmail.com>
2024-11-07 13:50:47 -0300

    CriacaoPerfil

---
+++ src/Pages/Perfil.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+
+
+export default function Home() {
+  return (
+    <View>
+        <Text>Olá</Text>
+    </View>
+  )
+}
