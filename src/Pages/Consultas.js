d8c377346ae66f868330b597bbac1da4ad30d4bb
dudah arruda <arrudamariaeduarda492@gmail.com>
2024-11-22 07:57:08 -0300

    Login Conectando

--- src/Pages/Consultas.js
+++ src/Pages/Consultas.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { View,  StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 
-export default function Consultas() {
+export default function Consultas({ navigation }) {
   return (
     <ScrollView contentContainerStyle={css.container}>
-        <View style={css.info} >
-                    <TouchableOpacity onPress={Voltar} style={css.Voltar}>
-                        <MaterialCommunityIcons name="arrow-left" size={30} />
-                    </TouchableOpacity>
-                </View>
+      <View style={css.info} >
+        <TouchableOpacity onPress={() => navigation.navigate("Home")} style={css.Voltar}>
+          <MaterialCommunityIcons name="arrow-left" size={30} />
+        </TouchableOpacity>
+      </View>
     </ScrollView >
   )
 }
 
 const css = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        backgroundColor: " #F4F4F4",
-        alignItems: "center",
-    }
-    
+  container: {
+    flexGrow: 1,
+    backgroundColor: " #F4F4F4",
+    alignItems: "center",
+  }
+
 })
 
